f0c5b01d651e46f10f82e256a100a4df29e37b58
Arij <arij.lemjid@etu.unice.fr>
2025-04-30 03:47:45 +0200

    new new

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -5,7 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {  //  <---  EXPORT is essential!
-  constructor(){}
-}
+export class AppComponent {
+  public title: string = 'Hello world!';
+  public showSuccess = false;
 
+  constructor() {}
+
+  showHideSuccess() {
+    this.showSuccess = !this.showSuccess;
+  }
+}
