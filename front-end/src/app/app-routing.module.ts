7f96f27c199a4a2e8eb35d9eec3ed688846e1562
Arij <arij.lemjid@etu.unice.fr>
2025-06-15 13:31:34 +0200

    ps

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,22 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ThemeSelectionComponent } from './theme-selection/theme-selection.component';
-import { GamePageComponent } from './game-page/game-page.component';
-import { QuizEndComponent } from './quiz-end-page/quiz-end-page.component';
-import { GameSettingsComponent } from './game-settings/game-settings.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
-import { AdminLoginComponent } from './admin-login/admin-login.component';
 
-const routes: Routes = [
-  { path: 'themes', component: ThemeSelectionComponent },
-  { path: 'game/:themeId', component: GamePageComponent }, 
-  { path: 'quiz-end', component: QuizEndComponent }, // AJOUTE CETTE LIGNE
-  { path: '', component: HomePageComponent },
-  { path: 'settings', component: GameSettingsComponent},
-  { path: 'admin-login', component: AdminLoginComponent},
-  { path: '', redirectTo: '/game', pathMatch: 'full' }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,44 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ThemeSelectionComponent } from './theme-selection/theme-selection.component';
-import { ThemeBoxComponent } from './theme-box/theme-box.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
 
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GamePageComponent } from './game-page/game-page.component';
-import { QuizEndComponent } from './quiz-end-page/quiz-end-page.component';
-import { GameSettingsComponent } from './game-settings/game-settings.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { MenuBarComponent } from './menu-bar/menu-bar.component';
-import { AdminLoginComponent } from './admin-login/admin-login.component';
-import { SettingsService } from './services/settings.service';
-import { QuestionService } from './services/question.service';
-import { ScoreService } from './services/score.service';
+
 @NgModule({
   declarations: [
-    AppComponent,
-    ThemeSelectionComponent,
-    ThemeBoxComponent,
-    GamePageComponent,
-    QuizEndComponent,
-    GameSettingsComponent,
-    HomePageComponent,
-    MenuBarComponent,
-    AdminLoginComponent
- 
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule,
-    AppRoutingModule,
-    ReactiveFormsModule , // Ajoutez-le à la liste des imports
-    FormsModule
-   
+    AppRoutingModule
   ],
-  providers: [SettingsService, QuestionService, ScoreService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
